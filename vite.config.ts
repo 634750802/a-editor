06232a6b98ab08b000facdb7e26a10b9da5a5e60
Jagger <634750802@qq.com>
2021-10-13 12:57:16 +0800

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,5 @@ import plainText from 'vite-plugin-plain-text'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      parserOpts: {
-        plugins: [
-          'decorators-legacy',
-        ],
-      },
-    },
-  }), plainText(/\.md$/)],
-  build: {},
-  esbuild: {},
-  resolve: {
-    alias: {
-      'node:url': 'url',
-      'universal-deep-strict-equal': 'src/shim/universal-deep-strict-equal.js'
-    },
-  },
-  define: {
-    process: {
-      env: {
-        'NODE_DEBUG': process.env.NODE_DEBUG,
-      },
-    },
-  },
+  plugins: [react({}), plainText(/\.md$/)]
 })
