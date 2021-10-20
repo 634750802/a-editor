0b214bc1e1443281dd0cbd467cdedd45e598d064
Jagger <634750802@qq.com>
2021-10-21 06:36:29 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default } from './components/ti-editor/TiEditor'
 export { default as layoutPlugin, VirtualSectionInput } from './plugins/layout'
-export { transformMarkdown } from './utils/compile'
+export { transformMarkdown, parseMarkdown } from './utils/compile'
--- src/utils/compile.ts
+++ src/utils/compile.ts
@@ -1,10 +1,11 @@
-import { slateToRemark } from 'remark-slate-transformer'
+import { remarkToSlate, slateToRemark } from 'remark-slate-transformer'
 import remarkGfm from 'remark-gfm'
 import remarkStringify from 'remark-stringify'
 import { unified } from 'unified'
 import { Descendant } from 'slate'
+import remarkParse from 'remark-parse'
 
-const processor = unified().use(slateToRemark).use(remarkGfm).use(remarkStringify, {
+const se = unified().use(slateToRemark).use(remarkGfm).use(remarkStringify, {
   emphasis: '*',
   strong: '*',
   listItemIndent: 'one',
@@ -12,9 +13,15 @@ const processor = unified().use(slateToRemark).use(remarkGfm).use(remarkStringif
   bullet: '-',
 }).freeze()
 
+const de = unified().use(remarkParse).use(remarkGfm).use(remarkToSlate).freeze()
+
 export function transformMarkdown (fragment: Descendant[]): string {
-  return processor.stringify(processor.runSync({
+  return se.stringify(se.runSync({
     type: 'root',
-    children: fragment
+    children: fragment,
   } as never))
+}
+
+export function parseMarkdown (value: string): Descendant[] {
+  return de.processSync(value).result as Descendant[]
 }
