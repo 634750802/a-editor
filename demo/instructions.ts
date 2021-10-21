edbc926b1decab757adad93f4b581a60473f2ee1
Jagger <634750802@qq.com>
2021-10-22 01:09:51 +0800

    fix: demo page

--- demo/instructions.ts
+++ demo/instructions.ts
@@ -1,5 +1,5 @@
 import { plainText as main } from '../readme.md'
 
-const docs = import.meta.globEager('../components/**/instructions.md')
+const docs = import.meta.globEager('../src/**/instructions.md')
 
 export const instructionsMd = main + '\n\n' + Object.values(docs).map(m => m.plainText).join('\n\n')
