5b8a21f9b14d576080ac20a89e02b051d0cd74c9
Jagger <634750802@qq.com>
2022-05-10 19:24:44 +0800

    fix: parse filename

--- packages/ti-editor/src/utils/blob.ts
+++ packages/ti-editor/src/utils/blob.ts
@@ -1,12 +1,22 @@
 // https://stackoverflow.com/questions/35940290/how-to-convert-base64-string-to-javascript-file-object-like-as-from-file-input-f
-export function urlToFile (base64: string): Promise<File> {
-  return fetch(base64)
+export function urlToFile (url: string): Promise<File> {
+  return fetch(url)
     .then(async res => {
       if (res.ok) {
-        return [await res.arrayBuffer(), res.headers.get('content-type') ?? undefined] as const;
+        const filename = getUrlName(url)
+        return [await res.arrayBuffer(), filename, res.headers.get('content-type') ?? undefined] as const;
       } else {
         return Promise.reject()
       }
     })
-    .then(([buf, type]) => new File([buf as ArrayBuffer], '', { type }))
+    .then(([buf, filename, type]) => new File([buf as ArrayBuffer], filename, { type }))
+}
+
+function getUrlName(url: string): string {
+  try {
+    const pathname = (new URL(url)).pathname
+    return pathname.split('/').reverse()[0] || 'unnamed'
+  } catch (e) {
+    return 'unnamed'
+  }
 }
